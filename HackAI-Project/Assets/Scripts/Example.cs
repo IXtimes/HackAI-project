4bedcaaafdc4b8d897b19773cb36c518a0ff750a
IXtimes <xandercorcoran@gmail.com>
2025-04-19 12:32:28 -0500

    Unity: Inital project commit.

---
+++ HackAI-Project/Assets/Scripts/Example.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+    public TextMeshProUGUI exampleText;
+    public float percentage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exampleText.text = "Your mom";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
