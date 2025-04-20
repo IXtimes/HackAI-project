9bb9c5035e25f754f45772ed260ca9ac743f298d
IXtimes <xandercorcoran@gmail.com>
2025-04-19 22:35:31 -0500

    Onboarding: Inital Commit.

--- HackAI-Project/Assets/Scripts/Onboarding/OnboardingManager.cs
+++ HackAI-Project/Assets/Scripts/Onboarding/OnboardingManager.cs
@@ -1,23 +1,116 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening.Plugins;
+using System.Linq;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
+[Serializable]
 public class OnboardingEvent {
     public string event_name;
-    public string event_index;
+    public int event_index;
 }
 public class OnboardingManager : MonoBehaviour
 {
     public string[] onboardingLines;
-    public OnboardingEvent[] oboardingEvents;
+    public OnboardingEvent[] onboardingEvents;
+    Dictionary<string, bool> onboardingEventStatus;
     int currentOnboardingLine;
+    Profile onboardedProfile;
     [SerializeField] TextMeshProUGUI onboardingText;
-    [SerializeField] Transform onboardingNameInput;
-    [SerializeField] PhotoAlbumInterface photoInput;
+    [SerializeField] GameObject onboardingNameInput;
+    [SerializeField] GameObject onboardingPhotoInput;
+    [SerializeField] GameObject onboardingSubmit;
+
+    void Awake()
+    {
+        // Hide all event related elements
+        onboardingNameInput.SetActive(false);
+        onboardingPhotoInput.SetActive(false);
+        onboardingSubmit.SetActive(false);
+
+        // Configure the base onboarding events
+        onboardingEventStatus = new Dictionary<string, bool>();
+        onboardingEventStatus["username"] = false;
+        onboardingEventStatus["profile_picture"] = false;
+        onboardingEventStatus["finish"] = false;
+
+        // Create an empty profile
+        onboardedProfile = new Profile();
+
+        // Set onboarding to the first line
+        onboardingText.text = onboardingLines[currentOnboardingLine];
+
+        // Automatically call the next onboarding line
+        DOTween.Sequence().AppendInterval(3f).AppendCallback(GetNextOnboardingLine);
+    }
+
+    public void CompleteEvent(string eventName) {
+        // Switch on the completed event
+        foreach(OnboardingEvent obEvent in onboardingEvents) { 
+            if(obEvent.event_name == eventName)
+                switch(obEvent.event_name) {
+                    case "username":
+                        // Set the username
+                        onboardingNameInput.SetActive(false);
+                        onboardedProfile.name = onboardingNameInput.transform.GetChild(0).GetComponent<TMP_InputField>().text;
+                        break;
+                    case "profile_picture":
+                        // Set the profile picture
+                        onboardingPhotoInput.SetActive(false);
+                        onboardedProfile.profilePicture = onboardingPhotoInput.GetComponent<PhotoAlbumInterface>().profileTexture;
+                        break;
+                    case "finish":
+                        onboardingSubmit.SetActive(false);
+                        break;
+                }
+        }
+
+        // Set that onboarding status to true, and trigger the next onboarding line
+        onboardingEventStatus[eventName] = true;
+
+        // Automatically fetch the next onboarding line.
+        GetNextOnboardingLine();
+    }
 
     public void GetNextOnboardingLine() {
+        // Check if there is an onboarding event here, and if so that its cleared
+        foreach(OnboardingEvent obEvent in onboardingEvents) {
+            if(obEvent.event_index == currentOnboardingLine && !onboardingEventStatus[obEvent.event_name])
+                // Void call until condition is met
+                return;
+            
+        }
+
+        // Move on to the next onboarding line
+        currentOnboardingLine++;
+
+        // If this is the last line of onboarding, stop the loop
+        if(onboardingLines.Length <= currentOnboardingLine) {
+            return;
+        }
+
         // Set the onboarding text to the next line of onboarding
+        onboardingText.text = onboardingLines[currentOnboardingLine].Replace("<name>", onboardedProfile.name);
+
+        // Enable the respective element based on the event 
+        foreach(OnboardingEvent obEvent in onboardingEvents) { 
+            if(obEvent.event_index == currentOnboardingLine)
+                switch(obEvent.event_name) {
+                    case "username":
+                        onboardingNameInput.SetActive(true);
+                        break;
+                    case "profile_picture":
+                        onboardingPhotoInput.SetActive(true);
+                        break;
+                    case "finish":
+                        onboardingSubmit.SetActive(true);
+                        break;
+                }
+        }
+
+        // Automatically call the next onboarding line
+        DOTween.Sequence().AppendInterval(3f).AppendCallback(GetNextOnboardingLine);
     }
 }
--- HackAI-Project/Assets/Scripts/Utility/PhotoAlbumInterface.cs
+++ HackAI-Project/Assets/Scripts/Utility/PhotoAlbumInterface.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
 
 public class PhotoAlbumInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public RawImage profilePreview; // Assign in Inspector
+    public Texture2D profileTexture;
+
+    public void PickImageFromGallery() {
+        // Prompt to get an image from the user's gallary
+        NativeGallery.GetImageFromGallery(path => {
+            profileTexture = LoadTextureFromFile(path);
+            profilePreview.texture = profileTexture;
+        }, title: "Profile Picture", mime: "image/*" );
     }
 
-    // Update is called once per frame
-    void Update()
+    public Texture2D LoadTextureFromFile(string path, int maxSize = 512)
     {
-        
+        if (!System.IO.File.Exists(path))
+        {
+            Debug.LogError("File does not exist at path: " + path);
+            return null;
+        }
+
+        byte[] imageData = System.IO.File.ReadAllBytes(path);
+        Texture2D texture = new Texture2D(2, 2); // Placeholder size, will auto-resize on LoadImage
+
+        if (texture.LoadImage(imageData))
+        {
+            // Optionally resize if needed here
+            return texture;
+        }
+
+        Debug.LogError("Failed to load image from data.");
+        return null;
     }
 }
